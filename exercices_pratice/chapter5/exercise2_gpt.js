bf736ebbc6aa0d3640474e2c3a383c8dacc9ce46
IgorSilvaZZ <igor06silva12@gmail.com>
2025-07-20 15:23:41 -0300

    feat: :heavy_plus_sign: resolved exercise 2 the gpt

--- exercices_pratice/chapter5/exercise2_gpt.js
+++ exercices_pratice/chapter5/exercise2_gpt.js
@@ -5,9 +5,45 @@
     Output: true
 */
 
-const isAnagram = (word1 = "", word2 = "") => {};
+const countFrequencyWord = (word = "") => {
+  let frequencyWord = {};
 
-const word1Param = "listen";
-const word2Param = "silent";
+  let normalizedWord = word.toLowerCase();
+
+  for (const letter of normalizedWord) {
+    if (!frequencyWord[letter]) {
+      frequencyWord[letter] = 1;
+    } else {
+      frequencyWord[letter] = frequencyWord[letter] + 1;
+    }
+  }
+
+  return frequencyWord;
+};
+
+const isAnagram = (word1 = "", word2 = "") => {
+  const countFrequencyWord1 = countFrequencyWord(word1.replaceAll(" ", ""));
+  const countFrequencyWord2 = countFrequencyWord(word2.replaceAll(" ", ""));
+
+  const keysWord1 = Object.keys(countFrequencyWord1);
+  const keysWord2 = Object.keys(countFrequencyWord2);
+
+  if (keysWord1.length !== keysWord2.length) {
+    return false;
+  }
+
+  for (const key of keysWord1) {
+    if (countFrequencyWord1[key] !== countFrequencyWord2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const word1Param = "hello";
+const word2Param = "helloo";
 
 const resultIsAnagram = isAnagram(word1Param, word2Param);
+
+console.log({ resultIsAnagram });
